5d25ca7b4eb8b662fd7c8b5e65816bd8c3cf7040
Björn Dahlgren <bjodah@gmail.com>
2016-03-12 16:49:28 +0100

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 # Usage:
 #
-#    $ ./scripts/release.sh v1.2.3
+#    $ ./scripts/release.sh v1.2.3 ~/anaconda2/bin
 #
 
 if [[ $1 != v* ]]; then
@@ -15,11 +15,27 @@ PKG=$(find . -maxdepth 2 -name __init__.py -print0 | xargs -0 -n1 dirname | xarg
 PKG_UPPER=$(echo $PKG | tr '[:lower:]' '[:upper:]')
 ./scripts/run_tests.sh
 env ${PKG_UPPER}_RELEASE_VERSION=$1 python setup.py sdist
-./scripts/build_conda_recipe.sh $1
+for CONDA_PY in 27 34 35; do
+    PATH=$2:$PATH CONDA_NPY=110 ./scripts/build_conda_recipe.sh $1
+done
 
 # All went well
 git tag -a $1 -m $1
 git push
 git push --tags
 twine upload dist/${PKG}-${1#v}.tar.gz
-echo "Remember to bump version (and commit and push)!"
+MD5=$(md5sum dist/${PKG}-$VERSION.tar.gz | cut -f1 -d' ')
+
+if [[ -d dist/conda-recipe-${1#v} ]]; then
+    rm -r dist/conda-recipe-${1#v}
+fi
+cp -r conda-recipe/ dist/conda-recipe-${1#v}
+sed -i -E -e "s/version:(.+)/version: $VERSION/" -e "s/path:(.+)/fn: $PKG-$VERSION.tar.gz\n    url: https:\/\/pypi.python.org\/packages\/source\/${PKG:0:1}\/$PKG\/$PKG-$VERSION.tar.gz#md5=$MD5\n    md5: $MD5/" -e '/cython/d' dist/conda-recipe-${1#v}/meta.yaml
+
+# Specific for this project:
+SERVER=hera
+scp -r dist/conda-recipe-${1#v}/ $PKG@$SERVER:~/public_html/conda-recipes/
+scp dist/${PKG}-$VERSION.tar.gz $PKG@$SERVER:~/public_html/releases/
+for CONDA_PY in 27 34 35; do
+    ssh $PKG@$SERVER "source /etc/profile; CONDA_PY=$CONDA_PY CONDA_NPY=110 conda-build ~/public_html/conda-recipes/conda-recipe-${1#v}/"
+done
