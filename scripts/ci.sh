ae80dbfb4af3c184c19f03e40345f80d482c2400
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-15 11:59:03 +0100

    pip install single tar.gz

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,7 +10,8 @@ source $(compgen -G "/opt-3/cpython-v3.*-apt-deb/bin/activate")
 #################### Install and test python package  ####################
 python3 -m pip install pytest-cov pytest-flakes matplotlib sphinx numpydoc sphinx_rtd_theme
 python3 setup.py sdist
-CC=gcc CXX=g++ python3 -m pip install dist/*.tar.gz  # --ignore-installed
+cd dist/
+CC=gcc CXX=g++ python3 -m pip install $(ls *.tar.gz | head -n 1)  # --ignore-installed
 (cd /; python3 -m pytest --pyargs $PKG_NAME -v)
 CC=gcc CXX=g++ python3 -m pip install -e .[all]
 PYTHONPATH=$(pwd) PYTHON=python3 ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
